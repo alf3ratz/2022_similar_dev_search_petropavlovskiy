b57d43859a1461d871c45d389aba94eeaa910bb4
alf3ratz <49417479+alf3ratz@users.noreply.github.com>
2022-06-10 02:12:01 +0300

    [gitbase-part] attempt to run gitbase

--- src/main.py
+++ src/main.py
@@ -1,22 +1,25 @@
 import fire
 
 from helpers import *
+from gitbase.aggregate_info import aggregate
 
 
 
-def get_repo(url, path):
+def get_repo():
     """
     Save data about repository to json file.
     :param path: path to JSON
     :param url: repository url
     :return: list of commits
     """
-    url = 'https://github.com/OmdenaAI/Arabic-Chapter'
-    path = path.replace('\\','\\\\')
-    with open(path) as file:
-        lines = file.readlines()
-        print(lines)
-    save_data(get_repository_info(url), path)
+    # url = 'https://github.com/OmdenaAI/Arabic-Chapter'
+    # path = path.replace('\\','\\\\')
+    # with open(path) as file:
+    #     lines = file.readlines()
+    #     print(lines)
+    # save_data(get_repository_info(url), path)
+    #get_stargazers("","",10)
+    aggregate()
 
 
 if __name__ == "__main__":
